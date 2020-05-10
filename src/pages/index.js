f3424354a91d09f73c154610afc9ddde0ecc4e2c
Cesar Murillo <namiayumis@gmail.com>
2020-05-10 19:52:01 +0900

    first query with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import ImagenHotel from "../components/imagenHotel"
 
 const indexPage = () => (
   <Layout>
-    <h1>Gatsby Hotel</h1>
+    <ImagenHotel />
   </Layout>
 )
 
