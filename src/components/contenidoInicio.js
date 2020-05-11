410e1616745bc8cb11cf0104fd51ccd41bb82ed3
Cesar Murillo <namiayumis@gmail.com>
2020-05-11 22:53:07 +0900

    background image Homepage

---
+++ src/components/contenidoInicio.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const contenidoInicio = () => {
+  return <div></div>
+}
+
+export default contenidoInicio
--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import styled from "@emotion/styled"
 
+const ImageBackground = styled(BackgroundImage)`
+  height: 700px;
+`
 const ImagenHotel = () => {
   const { image } = useStaticQuery(graphql`
     query {
       image: file(relativePath: { eq: "8.jpg" }) {
         sharp: childImageSharp {
           fluid {
-            srcSetWebp
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
     }
   `)
 
-  console.log(image)
+  // console.log(image.sharp.fluid);
 
-  return <h1>Imagen aqui</h1>
+  return (
+    <ImageBackground
+      tag="section"
+      fluid={image.sharp.fluid}
+      fadeIn="soft"
+    ></ImageBackground>
+  )
 }
 
 export default ImagenHotel
