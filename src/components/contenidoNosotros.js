abcd43b065e688508da4601b771add53a7000de4
Cesar Murillo <namiayumis@gmail.com>
2020-05-13 21:41:09 +0900

    about-us page css

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -1,7 +1,23 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
 
+const TextoInicio = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+  p {
+    line-height: 2;
+  }
+`
 const ContenidoNosotros = () => {
   const resultado = useStaticQuery(graphql`
     query {
@@ -23,11 +39,19 @@ const ContenidoNosotros = () => {
 
   return (
     <>
-      <h2>{titulo}</h2>
-      <div>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {titulo}
+      </h2>{" "}
+      <TextoInicio>
         <p> {contenido} </p>
         <Image fluid={imagen.fluid} />
-      </div>
+      </TextoInicio>
     </>
   )
 }
