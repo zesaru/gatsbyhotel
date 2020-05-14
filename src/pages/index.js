e5ca4a3963b91a7795ee7765977f568d6c35a9f5
Cesar Murillo <namiayumis@gmail.com>
2020-05-14 21:04:34 +0900

    create rooms component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,32 @@ import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
 import ContenidoInicio from "../components/contenidoInicio"
 import useHabitaciones from "../hooks/use-habitaciones"
+import { css } from "@emotion/core"
+import HabitacionPreview from "../components/habitacionPreview"
 
 const indexPage = () => {
-  //useHabitaciones()
+  const habitaciones = useHabitaciones()
+  console.log(habitaciones)
 
   return (
     <Layout>
       <ImagenHotel />
       <ContenidoInicio />
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >
+        Nuestras Habitaciones
+      </h2>
+
+      <ul>
+        {habitaciones.map(habitacion => (
+          <HabitacionPreview />
+        ))}
+      </ul>
     </Layout>
   )
 }
